a4e83655c61fbee1b30640e551f3371e5cf41f1a
Muhammet <33325253+blackdragonx61@users.noreply.github.com>
2019-08-23 00:41:08 +0300

    Add files via upload

---
+++ PART-3.cpp
@@ -0,0 +1,22 @@
+//this tutorial for clang-9 new
+
+cmd_gm.cpp
+//Find in: ACMD(do_set_stat)
+	
+	if (*szName == NULL || *szChangeAmount == '\0')
+	
+///Change
+	if (!*szName || *szChangeAmount == '\0')
+	
+char_skill.cpp
+	pkVictim->GetVID() : NULL
+	
+///change
+	pkVictim->GetVID() : 0
+	
+config.cpp
+//find
+	if (NULL != line[0])
+	
+///Change all
+	if (line[0])
